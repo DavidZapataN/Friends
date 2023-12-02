981c9c5fa972a294db03f1a9761538d327416c7c
DavidZapataN <dzapatan@unal.edu.co>
2023-12-01 22:34:43 -0500

    Seguimiento 6,Friends.

---
+++ src/main/java/com/mycompany/friends/Friends.java
@@ -0,0 +1,9 @@
+package com.mycompany.friends;
+
+public class Friends {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+       form.setVisible(true);
+    }
+}
